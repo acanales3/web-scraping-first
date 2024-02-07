ff6700786d5453528afd1176951b83efca303c00
acanales3 <alexander.s.canales@gmail.com>
2024-02-06 19:41:28 -0600

    UI (mostly) complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import ImageCarousel from "@/components/ImageCarousel";
+import SearchBar from "@/components/SearchBar";
 import Image from "next/image";
 
 const Home = () => {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
@@ -23,9 +25,9 @@ const Home = () => {
               Powerful, self-serve product and growth analytics to help you
               convert, engage, and retain more.
             </p>
-            SearchBar
+            <SearchBar />
           </div>
-          HeroCarosel
+          <ImageCarousel />
         </div>
       </section>
       <section className="trending-section">
